1c92026ac5cccf6f459d772c8ce1d18ae9b83fc8
Dani2097 <39124380+Dani2097@users.noreply.github.com>
2020-04-04 18:02:03 +0200

    Add files via upload

---
+++ FindJob/public/activate.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrea
+ * Date: 29/05/18
+ * Time: 23:43
+ */
+require '../DB/DBConnectionManager.php';
+require '../DB/DBUtenti.php';
+
+$email = $_GET['email'];
+$matricola = $_GET['matricola'];
+
+$db = new DBUtenti();
+$esito = "";
+($db->confermaProfilo($email, $matricola)) ? $esito = "Email confermata" : $esito = "Non è possibile confermare l'email ora, riprovare più tardi";
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<header>
+    <meta charset="UTF-8">
+    <title>Conferma email</title>
+</header>
+<body>
+    <h1><?php echo $esito?></h1>
+</body>
+</html>
+
